a543f66cbd26aa57b2c586f612b0ae8b6b5d55e4
Rastatrevftw1538 <trevor.cardoza1538@gmail.com>
2019-12-04 21:25:09 -0800

    Finally works well

--- Module 2 - Python/flaskapp/app/TebNoteApp.py
+++ Module 2 - Python/flaskapp/app/TebNoteApp.py
@@ -1,7 +1,9 @@
 # TEB Assignment Note
 
-import re
+import os
 import io
+import re
+
 import nltk
 
 from flask import Flask, render_template
@@ -9,7 +11,6 @@ from flask import Flask, render_template
 from nltk.corpus import wordnet
 
 import sys
-import os
 
 nltk.download('wordnet')
 
@@ -24,14 +25,19 @@ def main(Input):
     neg_num = 0
     count = 0
     #_word = input("Enter the word you want to search for: ")
-    emotion_words = {"happy": 0, "excited": 0,
-                     "sad": 0, "mad": 0}
+    emotion_words = {}
+    # emotion_words = {"happy":0,"excited":0,"good":0,
+    # "sad":0,"mad":0,"bad":0}
     new_corp = nltk.WordPunctTokenizer().tokenize(real_file)
     #_pos = nltk.pos_tag(nltk.word_tokenize(_word))
     # print(_pos)
     #thing = nltk.wsd.lesk(lang_,_word)
     # print(thing)
-    # print(new_corp)
+    #print(new_corp)
+
+    for thing in regex.finditer(corpusThing):
+        emotion_words[thing.group(1)] = thing.group(2)
+    #print(emotion_words)
 
     for x in new_corp:
         #print(x," word")
@@ -42,8 +48,8 @@ def main(Input):
                 for i in _syns.lemmas():
                     #print(i.name()," Synon")
                     if x == i.name() and times <= 0:
-                        print(x, " word_in_sent")
-                        print(i.name(), " matched syn")
+                        #print(x, " word_in_sent")
+                        #print(i.name(), " matched syn")
                         check = float(emotion_words[emo_wrd])
                         if check > 0:
                             pos_num += int(emotion_words[emo_wrd])
@@ -51,31 +57,25 @@ def main(Input):
                             neg_num += int(emotion_words[emo_wrd])
                         times += 1
                         break
-    pos_num = (emotion_words["happy"]+emotion_words["excited"])
-    neg_num = (emotion_words["sad"]+emotion_words["mad"])
-    note_thing = "Your text has found " + \
-        str(pos_num)+" instances of positivity and " + \
-        str(neg_num)+" instances of negativity."
-    if pos_num == 0 and neg_num == 0:
-        note_final = note_thing+"\n" + \
-            "We were unable to determine the emotion behind your text!"
-    elif pos_num <= 0 and neg_num >= 1:
-        neg_equation = 100
-        note_final = note_thing+"\n"+"Your notes are " + \
-            str(neg_equation)+"% percent negative."
-    elif neg_num <= 0 and pos_num >= 1:
-        pos_equation = 100
-        note_final = note_thing+"\n"+"Your notes are " + \
-            str(pos_equation)+"% percent positive."
-    elif pos_num >= 0 and neg_num >= 0:
-        pos_equation = int((pos_num)/((pos_num)+(neg_num))*(100))
-        neg_equation = int((neg_num)/((pos_num)+(neg_num))*(100))
-        note_thing2 = "Your notes are " + \
-            str(pos_equation)+"% percent positive and " + \
-            str(neg_equation)+"% percent negative"
+    #print(pos_num)
+    #print(neg_num)
+    total_polar = pos_num+neg_num
+    note_thing = "Your text has a polarity score of "+str(total_polar)
+    # if total_polar == 0:
+    #note_final = note_thing+"\n"+"We were unable to determine the emotion behind your text!"
+    # elif total_polar <= 0:
+    #neg_equation = 100
+    #note_final = note_thing+"\n"+"Your notes are "+str(neg_equation)+"% percent negative."
+    # elif neg_num <= 0 and pos_num >= 1:
+    #pos_equation = 100
+    #note_final = note_thing+"\n"+"Your notes are "+str(pos_equation)+"% percent positive."
+    # elif pos_num >= 0 and neg_num >= 0:
+    #pos_equation = int((pos_num)/((pos_num)+(neg_num))*(100))
+    #neg_equation = int((neg_num)/((pos_num)+(neg_num))*(100))
+    #note_thing2 = "Your notes are "+str(pos_equation)+"% percent positive and "+str(neg_equation)+"% percent negative"
 
     note_final = note_thing  # +"\n"+note_thing2
-    print(note_final)
+    #print(note_final)
     return note_final, Input
 
 
--- Module 2 - Python/flaskapp/app/app.py
+++ Module 2 - Python/flaskapp/app/app.py
@@ -1,5 +1,5 @@
 from flask import *
-from app.TebNoteApp import main
+from TebNoteApp import main
 import os
 from subprocess import *
 #import cardoza_make_a_move
@@ -8,50 +8,42 @@ app = Flask(__name__)
 
 notes = []
 
+
 @app.route('/')
 def start():
-  #return cardoza_make_a_move.do_the_thing()
-  return home()
+    # return cardoza_make_a_move.do_the_thing()
+    return Note()
 
-@app.route('/home')
-def home():
-  return render_template('home.html')
-  
-@app.route('/about')
-def about():
-  return render_template('about.html')
 
 @app.route('/layout')
 def layout():
-  return render_template('layout.html')
+    return render_template('layout.html')
 
-@app.route('/snake')
-def snake():
-  
-  return render_template('snake.html')
 
-@app.route('/NoteApp', methods=["GET","POST"])
+@app.route('/NoteApp', methods=["GET", "POST"])
 def Note():
-  if request.method == "POST":
-    note = request.form["Note"]
-    #Popen(["cd","app"])
-    #Popen(["python3","TebNoteApp.py","main","note"],stdout=PIPE ,stderr=STDOUT)
-    #print(note)
-    res_ = main(note)
-    return render_template('NoteApp.html', note=res_[0], copy=res_[1])
-  else:
-    return render_template('NoteApp.html')
+    if request.method == "POST":
+        note = request.form["Note"]
+        # Popen(["cd","app"])
+        #Popen(["python3","TebNoteApp.py","main","note"],stdout=PIPE ,stderr=STDOUT)
+        # print(note)
+        res_ = main(note)
+        return render_template('NoteApp.html', note=res_[0], copy=res_[1])
+    else:
+        return render_template('NoteApp.html')
 
-@app.route('/result', methods=["GET","POST"])
+
+@app.route('/result', methods=["GET", "POST"])
 def result():
-  if request.method == "POST":
-    note = request.form.get("Note")
-    #Popen(["cd","game/"],shell=True)
-    print(note)
-    Popen(["python3","TebNoteApp.py",],stdout=PIPE ,stderr=STDOUT)
-  #Popen(["main(Note)"])
+    if request.method == "POST":
+        note = request.form.get("Note")
+        # Popen(["cd","game/"],shell=True)
+        print(note)
+        Popen(["python3", "TebNoteApp.py", ], stdout=PIPE, stderr=STDOUT)
+    # Popen(["main(Note)"])
 
-  return render_template("result.html", name=note)
+    return render_template("result.html", name=note)
+
 
 if __name__ == '__main__':
-  app.run(host="0.0.0.0", port="8000", debug=False)
+    app.run(host="0.0.0.0", port="8000", debug=False)
